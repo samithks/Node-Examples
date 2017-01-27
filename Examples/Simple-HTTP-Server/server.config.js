f86bd3b2cbb42d24a81547435cf8e3fb7b0d384c
samithks <samithks@hotmail.com>
2017-01-27 16:16:00 +0530

    Comments added

--- Examples/Simple-HTTP-Server/server.config.js
+++ Examples/Simple-HTTP-Server/server.config.js
@@ -1,6 +1,9 @@
 'use strict';
 
+// Configuration for the server
 module.exports = {
+    // Port for the server
     port: process.env.PORT || 8080,
+    // Host for the server
     host: 'localhost'
 };
--- Examples/Simple-HTTP-Server/server.js
+++ Examples/Simple-HTTP-Server/server.js
@@ -1,12 +1,17 @@
 'use strict';
 
+// Import the HTTP Module
 const http = require('http');
+
+// Import the server config
 const config = require ('./server.config.js');
 
+// A function which handles requests and send response
 function handleRequest(req, res) {
     res.end('Hello World');
 }
 
+// Start a server that listen to the port in configuration file
 const server = http.createServer(handleRequest)
     .listen(config.port, config.host, function () {
         let host = server.address().address;
