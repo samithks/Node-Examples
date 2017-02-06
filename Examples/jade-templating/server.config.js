c8a85318e90e8ccbc63b2ab888db7bba89c0cff2
samithks <samithks@hotmail.com>
2017-02-06 23:52:33 +0530

    deleted

--- Examples/jade-templating/server.config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Configuration for the server
-module.exports = {
-    // Port for the server
-    port: process.env.PORT || 8080,
-    // Host for the server
-    host: 'localhost'
-};
